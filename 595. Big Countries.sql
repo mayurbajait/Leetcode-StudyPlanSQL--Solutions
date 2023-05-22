570bf4e4a0dd0f3b47a53fce51a14d10252df39a
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2023-05-22 16:59:07 +0530

    Update Second APproch

--- 595. Big Countries.sql
+++ 595. Big Countries.sql
@@ -1,3 +1,8 @@
 select name, population, area
 from World
 where (area>=3000000) or (population>=25000000)
+
+
+\\Second Approch
+select name, population, area from World 
+where area >= 3000000 or population >= 25000000
